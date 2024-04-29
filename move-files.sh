d6afe6e05bce58db340bfd3d1d4a64e3543baa2a
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-04-29 20:42:04 +0100

    Update move-files.sh

--- move-files.sh
+++ move-files.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Set the source directory and the destination directory
-src_dir=$HOME/Schreibtisch
-dst_dir=$HOME/obsidian/neogenesis
+src_dir="$HOME/Schreibtisch"
+dst_dir="$HOME/obsidian/neogenesis"
 
 # Loop through all Markdown files in the source directory
 for file in "$src_dir"/*.md; do
-  # Check if the file has the xattr 'user.tags' with value 'tomove'
-  if getfattr -d -m $file | grep -q "user.xdg.tags=movetoobsidian"; then
+  # Check if the file has the xattr 'user.xdg.tags' with value 'movetoobsidian'
+  if getfattr -n user.xdg.tags -e text "$file" | grep -q "movetoobsidian"; then
     # Move the file to the destination directory
     mv "$file" "$dst_dir"
   fi
-done 
+done
